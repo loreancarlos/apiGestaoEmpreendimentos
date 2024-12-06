6a9c30a565702c72f829f66227626d32ccd248be
Lorean Carlos Fernandes Soares <lorean.carlos@gmail.com>
2024-12-06 00:02:22 -0300

    Update server.js - last PORT

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { router } from './routes/index.js';
 
 const app = express();
-const port = process.env.SERVER_URL || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
