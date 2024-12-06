2806c6d3f81ef80e3a6ff424d3bb27e91954e5fc
Lorean Carlos Fernandes Soares <lorean.carlos@gmail.com>
2024-12-05 23:55:19 -0300

    Update server.js - NEW PORT

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-//import '../.env/config';
+import config from 'dotenv/config';
+
 import express from 'express';
 import cors from 'cors';
 import { router } from './routes/index.js';
 
 const app = express();
-const port = 5432;
+const port = process.env.SERVER_URL || 3000;
 
 app.use(cors());
 app.use(express.json());
