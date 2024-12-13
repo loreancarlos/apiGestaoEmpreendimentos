e8e05fe1c5a5cac8488f87036726d3738ad032a6
Lorean Carlos Fernandes Soares <lorean.carlos@gmail.com>
2024-12-13 15:54:51 -0300

    Update 20240311000002_create_clients_table.js

--- src/database/migrations/20240311000002_create_clients_table.js
+++ src/database/migrations/20240311000002_create_clients_table.js
@@ -6,8 +6,8 @@ export function up(knex) {
     table.string('email').notNullable();
     table.string('phone').notNullable();
     table.string('address').notNullable();
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('createdAt').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').notNullable().defaultTo(knex.fn.now());
   });
 }
 
